82e4e3563844304de00498c45e03263d61f722af
youssefeno <45043899+youssefeno@users.noreply.github.com>
2019-06-04 18:05:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`4play | Next Server`,"http://twitch.tv/Death Shop")
+client.user.setGame(`4play | "Last Music"`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
